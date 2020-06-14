d749dbbe198ec87e32b472c56ac2b4c6e146bdf1
Carlos Jimenez <cjimenez.diaz@protonmail.com>
2020-06-14 22:31:47 +0200

    Update Forecast-ARIMA.py

--- Forecast-ARIMA.py
+++ Forecast-ARIMA.py
@@ -42,87 +42,81 @@ plt.legend(['Training Set','Testing Data'])
 plt.show()
 
 # Tuning the AR Term #
-    # empty list to store aic/bic score
-    aic_p = []
-    bic_p = []
+aic_p = []
+bic_p = []
     
-    # p values
-    p = range(1,6) # [1,2,3,4,5]
+p = range(1,6) # [1,2,3,4,5]
     
-    # aic/bic score for different values of p
-    for i in p:
-        model = ARIMA(train_set, order=(i,1,0)) # define AR model
-        model_fit = model.fit(disp=-1) # fit the model
-        aic_temp = model_fit.aic # get aic score
-        bic_temp = model_fit.bic # get bic score
-        aic_p.append(aic_temp) # append aic score
-        bic_p.append(bic_temp) # append bic score
+# aic/bic score for different values of p
+for i in p:
+    model = ARIMA(train_set, order=(i,1,0)) # define AR model
+    model_fit = model.fit(disp=-1) # fit the model
+    aic_temp = model_fit.aic # get aic score
+    bic_temp = model_fit.bic # get bic score
+    aic_p.append(aic_temp) # append aic score
+    bic_p.append(bic_temp) # append bic score
     
-    # Plot of AIC/BIC score for AR term
-    plt.plot(range(1,6),aic_p, color='red')
-    plt.plot(range(1,6),bic_p)
-    plt.title('Tuning AR term')
-    plt.xlabel('p (AR term)')
-    plt.ylabel('AIC/BIC score')
-    plt.legend(['AIC score','BIC score'])
-    plt.show()
+# Plot of AIC/BIC score for AR term
+plt.plot(range(1,6),aic_p, color='red')
+plt.plot(range(1,6),bic_p)
+plt.title('Tuning AR term')
+plt.xlabel('p (AR term)')
+plt.ylabel('AIC/BIC score')
+plt.legend(['AIC score','BIC score'])
+plt.show()
 
     
 # Tuning the MA Term #
-  # empty list to store aic/bic score
-  aic_p = []
-  bic_p = []
-    
-  # p values
-  p = range(1,6) # [1,2,3,4,5]
-    
-  # aic/bic score for different values of p
-  for i in p:
-      model = ARIMA(train_set, order=(i,1,0)) # define AR model
-      model_fit = model.fit(disp=-1) # fit the model
-      aic_temp = model_fit.aic # get aic score
-      bic_temp = model_fit.bic # get bic score
-      aic_p.append(aic_temp) # append aic score
-      bic_p.append(bic_temp) # append bic score
-    
-  # Plot of AIC/BIC score for AR term
-  plt.plot(range(1,6),aic_p, color='red')
-  plt.plot(range(1,6),bic_p)
-  plt.title('Tuning AR term')
-  plt.xlabel('p (AR term)')
-  plt.ylabel('AIC/BIC score')
-  plt.legend(['AIC score','BIC score'])
-  plt.show()  
-    
+aic_p = []
+bic_p = []
 
+p = range(1,6) # [1,2,3,4,5]
+
+# aic/bic score for different values of p
+for i in p:
+    model = ARIMA(train_set, order=(i,1,0)) # define AR model
+    model_fit = model.fit(disp=-1) # fit the model
+    aic_temp = model_fit.aic # get aic score
+    bic_temp = model_fit.bic # get bic score
+    aic_p.append(aic_temp) # append aic score
+    bic_p.append(bic_temp) # append bic score
+    
+# Plot of AIC/BIC score for AR term
+plt.plot(range(1,6),aic_p, color='red')
+plt.plot(range(1,6),bic_p)
+plt.title('Tuning AR term')
+plt.xlabel('p (AR term)')
+plt.ylabel('AIC/BIC score')
+plt.legend(['AIC score','BIC score'])
+plt.show()  
+   
 # Fitting ARIMA model
 model = ARIMA(train_set, order=(2,1,2))
 model_fit_0 = model.fit()
 model_fit_0
 
-
 # Forecast using ARIMA model
-    past = train_set.tolist()
-    predictions = []
+past = train_set.tolist()
+predictions = []
     
-    for i in range(len(test_set)):
-        model = ARIMA(past, order=(2,1,2)) 
-        model_fit = model.fit(disp=-1, start_params=model_fit_0.params) l
-        forecast_results = model_fit.forecast() 
+for i in range(len(test_set)):
+    model = ARIMA(past, order=(2,1,2)) 
+    model_fit = model.fit(disp=-1, start_params=model_fit_0.params)
+    forecast_results = model_fit.forecast() 
         
-        pred = forecast_results[0][0] 
-        predictions.append(pred) 
-        past.append(test_set[i]) 
+    pred = forecast_results[0][0] 
+    predictions.append(pred) 
+    past.append(test_set[i]) 
     
-    # calculate mse
-    error = mean_squared_error(test_set, predictions)
-    print('Test MSE: {mse}'.format(mse=error))
+# calculate mse
+error = mean_squared_error(test_set, predictions)
+print('Test MSE: {mse}'.format(mse=error))
 
-    # Plot forecasted and actual values
-    plt.plot(test_set)
-    plt.plot(test_set.index,predictions, color='red')
-    plt.title('Visa Stock Price')
-    plt.xlabel('Date')
-    plt.ylabel('Price in dollars')
-    plt.legend(['test_set','predictions'])
-    plt.show()
+# Plot forecasted and actual values
+plt.plot(test_set)
+plt.plot(test_set.index,predictions, color='red')
+plt.title('Visa Stock Price')
+plt.xlabel('Date')
+plt.ylabel('Price in dollars')
+plt.legend(['test_set','predictions'])
+plt.show()
